c81421781baeba7ca9f52cd74600a14361422fb7
echen <eric.c@utexas.edu>
2019-04-11 16:41:53 -0500

    first commit

---
+++ Lab8.c
@@ -0,0 +1,133 @@
+// Lab8.c
+// Runs on LM4F120 or TM4C123
+// Student names: change this to your names or look very silly
+// Last modification date: change this to the last modification date or look very silly
+// Last Modified: 11/6/2018 
+
+// Specifications:
+// Measure distance using slide pot, sample at 60 Hz
+// maximum distance can be any value from 1.5 to 2cm
+// minimum distance is 0 cm
+// Calculate distance in fixed point, 0.001cm
+// Analog Input connected to PD2=ADC5
+// displays distance on Sitronox ST7735
+// PF3, PF2, PF1 are heartbeats (use them in creative ways)
+// 
+
+#include <stdint.h>
+
+#include "ST7735.h"
+#include "TExaS.h"
+#include "ADC.h"
+#include "print.h"
+#include "../inc/tm4c123gh6pm.h"
+
+//*****the first three main programs are for debugging *****
+// main1 tests just the ADC and slide pot, use debugger to see data
+// main2 adds the LCD to the ADC and slide pot, ADC data is on ST7735
+// main3 adds your convert function, position data is no ST7735
+
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+
+#define PF1       (*((volatile uint32_t *)0x40025008))
+#define PF2       (*((volatile uint32_t *)0x40025010))
+#define PF3       (*((volatile uint32_t *)0x40025020))
+// Initialize Port F so PF1, PF2 and PF3 are heartbeats
+void PortF_Init(void){
+	volatile uint32_t delay;
+	SYSCTL_RCGCGPIO_R |= 0x20;
+	delay = 100;
+	delay++;
+	GPIO_PORTF_DIR_R |= 0x0E;
+	GPIO_PORTF_DEN_R |= 0x0E;
+}
+
+void SysTick_Init(void){
+	// write this
+	NVIC_ST_CTRL_R = 0;
+	NVIC_ST_RELOAD_R = 0x145855;
+	NVIC_ST_CURRENT_R = 0;
+	
+	NVIC_ST_CTRL_R = 0x7;
+}
+
+uint32_t Data;        // 12-bit ADC
+uint32_t Position;    // 32-bit fixed-point 0.001 cm
+uint32_t ADCmail;
+int8_t ADCstatus;
+/*
+int main(void){       // single step this program and look at Data
+  TExaS_Init();       // Bus clock is 80 MHz 
+  ADC_Init();         // turn on ADC, set channel to 5
+  while(1){                
+    Data = ADC_In();  // sample 12-bit channel 5
+  }
+}
+
+int main2(void){
+  TExaS_Init();       // Bus clock is 80 MHz 
+  ADC_Init();         // turn on ADC, set channel to 5
+  ST7735_InitR(INITR_REDTAB); 
+  PortF_Init();
+  while(1){           // use scope to measure execution time for ADC_In and LCD_OutDec           
+    PF2 = 0x04;       // Profile ADC
+    Data = ADC_In();  // sample 12-bit channel 5
+    PF2 = 0x00;       // end of ADC Profile
+    ST7735_SetCursor(0,0);
+    PF1 = 0x02;       // Profile LCD
+    LCD_OutDec(Data); 
+    ST7735_OutString("    ");  // spaces cover up characters from last output
+    PF1 = 0;          // end of LCD Profile
+  }
+}
+*/
+// your function to convert ADC sample to distance (0.001cm)
+uint32_t Convert(uint32_t input){
+  return input;
+}
+void SysTick_Handler(void){
+	GPIO_PORTF_DATA_R ^=0x02;
+	GPIO_PORTF_DATA_R ^=0x02;
+	ADCstatus = 1;
+	ADCmail = ADC_In();
+}
+/*
+int main3(void){ 
+  TExaS_Init();         // Bus clock is 80 MHz 
+  ST7735_InitR(INITR_REDTAB); 
+  PortF_Init();
+  ADC_Init();         // turn on ADC, set channel to 5
+  while(1){  
+    PF2 ^= 0x04;      // Heartbeat
+    Data = ADC_In();  // sample 12-bit channel 5
+    PF3 = 0x08;       // Profile Convert
+    Position = Convert(Data); 
+    PF3 = 0;          // end of Convert Profile
+    PF1 = 0x02;       // Profile LCD
+    ST7735_SetCursor(0,0);
+    LCD_OutDec(Data); ST7735_OutString("    "); 
+    ST7735_SetCursor(6,0);
+    LCD_OutFix(Position);
+    PF1 = 0;          // end of LCD Profile
+  }
+}   */
+int main(void){
+  PortF_Init();
+	TExaS_Init();
+  // your Lab 8
+  EnableInterrupts();
+	SysTick_Init();
+	ADC_Init();
+	ST7735_InitR(INITR_REDTAB); 
+  ADCstatus = -1;
+	while(1){
+		while(ADCstatus<0){};
+		LCD_OutFix(Convert(ADCmail));
+		ST7735_OutChar('\n');
+		ADCstatus = -1;
+  }
+}
+
+
+
---
+++ Print.s
@@ -0,0 +1,129 @@
+; Print.s
+; Student names: change this to your names or look very silly
+; Last modification date: change this to the last modification date or look very silly
+; Runs on LM4F120 or TM4C123
+; EE319K lab 7 device driver for any LCD
+;
+; As part of Lab 7, students need to implement these LCD_OutDec and LCD_OutFix
+; This driver assumes two low-level LCD functions
+; ST7735_OutChar   outputs a single 8-bit ASCII character
+; ST7735_OutString outputs a null-terminated string 
+
+    IMPORT   ST7735_OutChar
+    IMPORT   ST7735_OutString
+    EXPORT   LCD_OutDec
+    EXPORT   LCD_OutFix
+
+    AREA    |.text|, CODE, READONLY, ALIGN=2
+    THUMB
+rmd EQU 0 ;32 bit unsigned dec
+dec EQU 4
+
+;-----------------------LCD_OutDec-----------------------
+; Output a 32-bit number in unsigned decimal format
+; Input: R0 (call by value) 32-bit unsigned number
+; Output: none
+; Invariables: This function must not permanently modify registers R4 to R11
+LCD_OutDec
+	PUSH{R0,LR}
+	SUB SP,#8 ;allocate local var
+	MOV R3,#10
+	CMP R0,R3
+	
+	BLO baseCase
+	UDIV R1,R0,R3 ;R1 contains the MSV
+	MLS R2,R1,R3,R0 ;R2 now contains remainder. this instruction does r0-r1*r3
+	MOV R0,R1 ;R0 contains new value
+	STR R2,[SP,#rmd] ;put remainder on stack
+	BL LCD_OutDec ;call itself again
+	;***return path***
+	
+	LDR R0,[SP,#rmd]
+	
+	
+baseCase ADD R0,#0x30
+	BL ST7735_OutChar
+
+	;****restore stack****
+	ADD SP,#8
+	POP {R0,LR}
+
+      BX  LR
+;* * * * * * * * End of LCD_OutDec * * * * * * * *
+
+; -----------------------LCD _OutFix----------------------
+; Output characters to LCD display in fixed-point format
+; unsigned decimal, resolution 0.001, range 0.000 to 9.999
+; Inputs:  R0 is an unsigned 32-bit number
+; Outputs: none
+; E.g., R0=0,    then output "0.000 "
+;       R0=3,    then output "0.003 "
+;       R0=89,   then output "0.089 "
+;       R0=123,  then output "0.123 "
+;       R0=9999, then output "9.999 "
+;       R0>9999, then output "*.*** "
+; Invariables: This function must not permanently modify registers R4 to R11
+LCD_OutFix
+	PUSH{R0,LR}
+	SUB SP,#8
+	
+	LDR R1,=9999
+	CMP R0,R1
+	BHI ovf
+	LDR R1,=1000
+	UDIV R2,R0,R1 ;R2 contains the thousands place 
+	MLS R3,R2,R1,R0 ;R3 contains remainder
+	STR R3,[SP,#dec]
+	MOV R0,R2
+	BL LCD_OutDec ;should print out thousands place
+	
+	
+	LDR R0,=0x2E 
+	BL ST7735_OutChar ;should output a decimal
+	
+	LDR R0,[SP,#dec] ;takes value, max 999
+	LDR R1,=100
+	UDIV R2,R0,R1 ;divides it by 100, leaving hundreds place
+	MLS R3,R2,R1,R0 ;we find the remainder, which is impt now
+	STR R3,[SP,#rmd] ;R3 is stored as the remainder
+	MOV R0,R2 ;divided value is loaded into R0
+	ADD R0,R0,#0x30  ;convert to ASCII
+	BL ST7735_OutChar ;should output hundreds digit, then subtract, leaving tens digit
+	
+	LDR R0,[SP,#rmd] ;take the remainder
+	LDR R1,=10 
+	UDIV R2,R0,R1 ;divide it by 10
+	MLS R3,R1,R2,R0 ;find the new remainder
+	STR R3,[SP,#rmd] ;store it
+	MOV R0,R2 ;Move divided value into R0, prepare for output
+	ADD R0,R0,#0x30 ;convert to ASCII
+	BL ST7735_OutChar ;spit out
+	
+	LDR R0,[SP,#rmd] ;take final remainder. it is already a unit digit
+	ADD R0,R0,#0x30 ;convert to ASCII
+	BL ST7735_OutChar
+	
+	
+	BL exitoutfix
+	
+ovf MOV R0,#0x2A
+	BL ST7735_OutChar
+	MOV R0,#0x2E
+	BL ST7735_OutChar
+	MOV R0,#0x2A
+	BL ST7735_OutChar
+	MOV R0,#0x2A
+	BL ST7735_OutChar
+	MOV R0,#0x2A
+	BL ST7735_OutChar
+	
+exitoutfix	ADD SP,#8
+		POP{R0,LR}
+	
+ BX   LR
+ 
+     ALIGN
+;* * * * * * * * End of LCD_OutFix * * * * * * * *
+
+     ALIGN                           ; make sure the end of this section is aligned
+     END                             ; end of file
